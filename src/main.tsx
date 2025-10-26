4162aa440d91f8a820a93d772c49db850aa06d23
Maksim <70702008m@gmail.com>
2025-10-26 20:39:04 +0200

    text

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-// main.tsx
+// src/main.tsx
 
 import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
-    <p>
-      This is JSX — it looks like HTML, but it's not quite the same. It has its
-      own rules!
-    </p>
-  </div>
-);
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
