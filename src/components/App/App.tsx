43cc7563e1f0f57113b509cf2f7d12c7951ce0b8
Maksim <70702008m@gmail.com>
2025-11-01 22:37:46 +0200

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,13 +5,7 @@ import css from "./App.module.css";
 export default function App() {
   return (
     <div className={css.app}>
-      <div className={css.container}>
-        <h1 className={css.title}>☕ Sip Happens Café</h1>
-        <p className={css.description}>
-          Please rate our service by selecting one of the options below.
-        </p>
-
-               <CafeInfo />
+                     <CafeInfo />
 
         <div className={css.buttons}>
           <button className={css.button}>Good</button>
@@ -19,7 +13,6 @@ export default function App() {
           <button className={css.button}>Bad</button>
           <button className={css.button}>Reset</button>
         </div>
-      </div>
-    </div>
+          </div>
   );
 }
