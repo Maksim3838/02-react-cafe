a25e8f147e4085284857569d81d998d9b622d0b8
Maksim <70702008m@gmail.com>
2025-11-01 22:09:50 +0200

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,34 @@
+// import css from "./App.module.css";
+
+// // import VoteOptions from "../VoteOptions";
+// // import VoteStats from "../VoteStats";
+// // import Notification from "../Notification";
+
+
+// export default function App ()
+//  {return (
+//  <div className={css.app}></div>);
+
+//  }
+
 import css from "./App.module.css";
 
-// import VoteOptions from "../VoteOptions";
-// import VoteStats from "../VoteStats";
-// import Notification from "../Notification";
+export default function App() {
+  return (
+    <div className={css.app}>
+      <div className={css.container}>
+        <h1 className={css.title}>☕ Sip Happens Café</h1>
+        <p className={css.description}>
+          Please rate our service by selecting one of the options below.
+        </p>
 
-
-export default function App ()
- {return (
- <div className={css.app}></div>);
-
- }
+        <div className={css.buttons}>
+          <button className={css.button}>Excellent</button>
+          <button className={css.button}>Good</button>
+          <button className={css.button}>Fair</button>
+          <button className={css.button}>Poor</button>
+        </div>
+      </div>
+    </div>
+  );
+}
