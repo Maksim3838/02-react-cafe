4d3559a0154f41acd9bbe411d54e5ed69bfa2d5c
Maksim <70702008m@gmail.com>
2025-11-01 07:45:03 +0200

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,9 @@
 import css from "./App.module.css";
-import Cafeinfo from "../CafeInfo/Cafeinfo";
-import VoteOptions from "../VoteOptions";
-import VoteStats from "../VoteStats";
-import Notification from "../Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
+// import VoteOptions from "../VoteOptions";
+// import VoteStats from "../VoteStats";
+// import Notification from "../Notification";
 
 
-const jsxMarkup = (
-  <div className={css.app}></div>
 
-);
 
-createRoot(document.getElementById("root") as HTMLElement).render(jsxMarkup);
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
 import "./global.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
