09e7d10261c9fe64491f7ff1cc32707c7311aab1
Maksim <70702008m@gmail.com>
2025-11-01 07:57:32 +0200

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,13 @@
 import css from "./App.module.css";
-import CafeInfo from "../Cafelnfo/Cafelnfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
+
 // import VoteOptions from "../VoteOptions";
 // import VoteStats from "../VoteStats";
 // import Notification from "../Notification";
 
 
+export default function App ()
+ {return (
+ <div className={css.app}></div>);
 
-
+ }
